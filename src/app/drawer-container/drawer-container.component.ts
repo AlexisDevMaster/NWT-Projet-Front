1678a60584346d7e93785a7e832dd19d7e59fc39
TheLuckyDev <theluckydev@gmail.com>
2020-11-21 03:37:27 +0100

    Add Drawer

---
+++ src/app/drawer-container/drawer-container.component.ts
@@ -0,0 +1,25 @@
+import {Component, Input, OnChanges, OnInit, ViewChild} from '@angular/core';
+import {MatSidenav} from '@angular/material/sidenav';
+
+@Component({
+  selector: 'nwt-drawer-container',
+  templateUrl: './drawer-container.component.html',
+  styleUrls: ['./drawer-container.component.css']
+})
+export class DrawerContainerComponent implements OnChanges {
+  @ViewChild('sidenav') public sidenav: MatSidenav;
+
+  @Input()
+  openNav: boolean;
+  constructor() { }
+
+  ngOnChanges(): void {
+    console.log('ngOnChanges', this.openNav);
+    if (this.openNav) {
+      this.sidenav.open();
+    } else {
+      this.sidenav.close();
+    }
+  }
+
+}
