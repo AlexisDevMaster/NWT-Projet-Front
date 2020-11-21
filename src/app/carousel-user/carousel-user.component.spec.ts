313da5039229fbbf5856eee2ecdc571d0d778847
TheLuckyDev <theluckydev@gmail.com>
2020-11-21 22:38:13 +0100

    Add carousel for users

---
+++ src/app/carousel-user/carousel-user.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CarouselUserComponent } from './carousel-user.component';
+
+describe('CarouselUserComponent', () => {
+  let component: CarouselUserComponent;
+  let fixture: ComponentFixture<CarouselUserComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CarouselUserComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CarouselUserComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
