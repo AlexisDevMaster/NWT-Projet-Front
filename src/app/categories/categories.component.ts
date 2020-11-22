f7e68134fbeafcc6fe6997703fb93886795dcafe
TheLuckyDev <theluckydev@gmail.com>
2020-11-22 23:38:17 +0100

    Add Categories list page

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {CategoryService} from '../shared/services/category.service';
+import {Category} from '../shared/interfaces/category';
+
 
 @Component({
   selector: 'nwt-categories',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor() { }
+  private _categories: Category[];
+
+  constructor(private _router: Router, private _categoriesService: CategoryService) {
+    this._categories = [];
+  }
+
+  /**
+   * Returns private property _categories
+   */
+  get categories(): Category[] {
+    return this._categories;
+  }
+
 
   ngOnInit(): void {
+    this._categoriesService
+      .fetch().subscribe((categories: Category[]) => {
+      this._categories = categories;
+    });
+  }
+
+  /**
+   * Function to navigate to current category
+   */
+  navigate(id: string): void {
+    this._router.navigate([ '/category', id ]);
   }
 
 }
